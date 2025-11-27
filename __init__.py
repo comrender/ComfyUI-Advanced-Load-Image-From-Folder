ddc690c23aa714c72139d3af7e171221cb12187d
comrender <139063732+comrender@users.noreply.github.com>
2025-11-27 18:33:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .advanced_load import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .advanced_load_image_from_folder import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
